6a2e1f925da14e03abb97c723d7874088eaad8c7
Illya Bendik <33512264+illyaBendik@users.noreply.github.com>
2024-08-01 17:43:17 +0200

    add dist

--- src/components/icons/CloseIcon.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="1.6em" height="1.5em" viewBox="0 0 32 32">
-    <rect width="32" height="32" fill="none" />
-    <path
-      fill="currentColor"
-      d="M17.414 16L24 9.414L22.586 8L16 14.586L9.414 8L8 9.414L14.586 16L8 22.586L9.414 24L16 17.414L22.586 24L24 22.586z"
-    />
-  </svg>
-</template>
