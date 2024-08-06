231a59a9cfed9b7c2299880d5afd32f7165928b5
Illya Bendik <33512264+illyaBendik@users.noreply.github.com>
2024-08-06 15:28:19 +0200

    update

--- src/views/PrivacyPolicyView.vue
+++ src/views/PrivacyPolicyView.vue
@@ -3,7 +3,7 @@
     <div class="box mx-auto px-6 pt-4 pb-20 md:pb-0 md:pt-0 md:px-16 text-base text-black-N900">
       <div class="grid md:grid-cols-2 gap-5 pt-10 items-center">
         <div class="order-last md:order-first">
-          <h1 class="headline mb-8">Privacy Policy</h1>
+          <h1 class="text-[32px] mb-8">Privacy Policy</h1>
           <p class="text-xl pb-4">Allgemeine Informationen zur Datenverarbeitung</p>
           <p class="font-medium">Verantwortliche Stelle:</p>
           <p>WhatsGreen Research & Science GmbH</p>
